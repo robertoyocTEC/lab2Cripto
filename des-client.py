7b0172dc108ba1d563da460bca3bb43e0332fb45
Roberto Yoc <roberto.yoc@energyza.energy>
2020-03-24 20:38:59 -0600

    encoded to bytes message

--- des-client.py
+++ des-client.py
@@ -1,6 +1,7 @@
 from Crypto.Cipher import DES
 from Crypto.Util.Padding import pad
 from socket import socket, SOCK_STREAM, AF_INET
+import json
 
 
 key = b'mycustom'
@@ -19,7 +20,9 @@ send = {
     'iv': cipher.iv
 }
 
+send_encoded = json.dumps(send).encode('ascii')
+
 with socket(AF_INET, SOCK_STREAM) as s:
     s.connect(("192.168.0.10", 1100))
-    data = s.send(send)
+    data = s.send(send_encoded)
     s.close()
--- des-server.py
+++ des-server.py
@@ -1,7 +1,7 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from Crypto.Cipher import DES
 from Crypto.Util.Padding import unpad
-
+import json
 
 
 
@@ -12,7 +12,9 @@ with socket(AF_INET, SOCK_STREAM) as s:
     s.listen(1)
     connection,address = s.accept()
     with connection:
-        a = connection.recv(1024)
+        receipt = connection.recv(1024)
+        a = json.loads(receipt)
+        
         cipher = DES.new(key, DES.MODE_CBC, a.iv)
         message = unpad(cipher.decrypt(a.message), 8)
         print("The message was: ", message)
